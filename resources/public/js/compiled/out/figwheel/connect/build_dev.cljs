0445fb8afa4ece5d8c08d9935a3195655140be49
Fed Reggiardo <bksgtr@gmail.com>
2017-07-06 13:14:57 -0500

    Removed compiled files

--- resources/public/js/compiled/out/figwheel/connect/build_dev.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect.build-dev (:require [figwheel.client] [tabber.core] [figwheel.client.utils]))
-(figwheel.client/start {:on-jsload (fn [& x] (if js/tabber.core.on-js-reload (apply js/tabber.core.on-js-reload x) (figwheel.client.utils/log :debug "Figwheel: :on-jsload hook 'tabber.core/on-js-reload' is missing"))), :open-urls ["http://localhost:3449/index.html"], :build-id "dev", :websocket-url "ws://localhost:3451/figwheel-ws"})
-
