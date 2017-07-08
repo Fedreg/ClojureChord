60100ca8c8deea6af513c67082ad9acccc70edf2
Fed Reggiardo <bksgtr@gmail.com>
2017-07-08 16:18:52 -0500

    Added 6th chords

--- src/tabber/core.cljs
+++ src/tabber/core.cljs
@@ -125,7 +125,7 @@
 	["All" "A" "Ab" "B" "Bb" "C" "C#" "D" "Db" "E" "Eb" "F" "F#""G" "G#"])
 
 (def qualityList
-	["All" "M" "m" "7" "M7" "m7"])
+	["All" "M" "m" "6" "7" "M7" "m7"])
 
 (defn KeyFilter [collection]
 	(let [key (:key @state/app-state) quality (:quality @state/app-state)]
