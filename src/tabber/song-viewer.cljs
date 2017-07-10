c57992423637a38c3df5ac6a7a0da8a1131d2ede
Fed Reggiardo <bksgtr@gmail.com>
2017-07-09 23:26:44 -0500

    Added beat indictor to each chord (wip)

--- src/tabber/song-viewer.cljs
+++ src/tabber/song-viewer.cljs
@@ -3,11 +3,40 @@
               [tabber.colorThemes :as color]
               [tabber.chordChart :as chart]))
 
-(defn StartSong []
-	(if (< (:index @state/app-state) (count (:song @state/app-state)))
-		(js/setTimeout #((swap! state/app-state update-in [:index] inc) (StartSong)) (* 1000 (last (nth (:song @state/app-state) (:index @state/app-state)))))))
+(enable-console-print!)
+(defn StartBeatCounter []
+    (let [song (:song @state/app-state)
+            beat (:beat @state/app-state)
+            index (:index @state/app-state)
+            numberOfBeats (->> (nth song index)
+                            (last))]
+    (print beat numberOfBeats index)
+    (if (<= beat numberOfBeats)
+        (js/setTimeout #((swap! state/app-state update-in [:beat] inc) (StartBeatCounter)) 1000)
+        (swap! state/app-state assoc-in [:beat] 1))))
 
-(defn StartCounter [])
+(defn StartSong []
+    (let [song (:song @state/app-state)
+            index (:index @state/app-state)]
+	(if (< index (count song))
+		(js/setTimeout #((swap! state/app-state update-in [:index] inc) (StartSong) (StartBeatCounter)) (* 1000 (last (nth song index)))))))
+
+(defn BeatCounter []
+    (let [beat (:beat @state/app-state)
+            index (:index @state/app-state)
+            song (:song @state/app-state)
+            range (->> (nth song index)
+                    (last)
+                    (inc)
+                    (range 1))]
+    [:div {:style {:position "fixed" 
+                    :bottom "25px" 
+                    :left "25px" 
+                    :display "flex" 
+                    :fontSize "50px" }}
+        (map 
+            (fn [e] [:div {:style {:paddingLeft "10px" 
+                                    :color (if (= e beat) (color/ReturnColors :f1) (color/ReturnColors :t2) )}} e]) range)]))
 
 (defn SongChordFilter [collection song index]
 	(let [key (first (nth song index)) quality (second (nth song index))]
@@ -18,7 +47,10 @@
 			(map chart/ChordChart (SongChordFilter (:chords @state/app-state) (:song @state/app-state) (:index @state/app-state)))])
 
 (defn OnDeckChord [num upper]
-    [:div {:style {:transform "scale(0.5)" :position "fixed" :top upper :right "50px"}}
+    [:div {:style {:transform "scale(0.9)" 
+                    :position "fixed" 
+                    :top upper 
+                    :right "150px"}}
 			(if (< (+ num (:index @state/app-state)) (count(:song @state/app-state)))
 				(map chart/ChordChart (SongChordFilter (:chords @state/app-state) (:song @state/app-state) (+ num (:index @state/app-state))))
 				"")])
@@ -30,4 +62,9 @@
 		[CurrentChord]
 		[OnDeckChord 1 "100px"]
         [OnDeckChord 2 "300px"]
-		[:button {:on-click #(do (swap! state/app-state assoc-in [:index] 1) (StartSong) (StartCounter)) :style {:position "fixed" :bottom "25px" :left "50%"}} "START"]])
+        [BeatCounter]
+		[:button {:on-click #(do (swap! state/app-state assoc-in [:index] 1) (StartSong) (StartBeatCounter)) 
+            :style {:position "fixed" 
+                    :bottom "25px" 
+                    :left "50%"}} "START"]])
+
--- src/tabber/state.cljs
+++ src/tabber/state.cljs
@@ -1,13 +1,17 @@
 (ns tabber.state
     (:require [reagent.core :as reagent :refer [atom]]
                 [tabber.chords :as chords]))
- 
+(def HeyJude '("Hey Jude" 
+    ("D" "M" 4) ("A" "M" 4) ("A" "7" 4) ("D" "M" 4) ("G" "M" 4) ("D" "M" 4) ("A" "7" 4) ("D" "M" 4)
+    ("D" "M" 4) ("A" "M" 4) ("A" "7" 4) ("D" "M" 4) ("G" "M" 4) ("D" "M" 4) ("A" "7" 4) ("D" "M" 4)))
+
 (defonce app-state (atom {:chords chords/chordList 
                             :key "A" 
+                            :beat 1
                             :quality "All"
                             :showModal false
                             :index 1
-                            :song '("Hey Jude" ("D" "M" 4) ("A" "M" 4) ("A" "7" 4) ("D" "M" 4) ("G" "M" 4) ("D" "M" 4) ("A" "7" 4) ("D" "M" 4))
+                            :song HeyJude 
                             :currentPage "Chord Charts"
                             :tempo 80
                             :colors "dark"}))
